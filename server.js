17b586f9593b843d0d119d82ebf34cf5a472eb13
Ivan Sharapov <oldrum75@gmail.com>
2023-12-13 21:54:49 +0100

    fix: add readme and server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Установка директории, где находится файл index.html
+app.use(express.static(path.join(__dirname, './')));
+
+// Обработка запроса к файлу index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+// Запуск сервера на порту 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту http://localhost:3000/ ${PORT}`);
+});
